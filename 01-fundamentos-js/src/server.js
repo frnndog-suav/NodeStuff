261e276fe76b1eefaac9800d54ab7b9cf90c6bf1
Fernando Goia <f97.goya@gmail.com>
2024-03-20 20:54:08 -0300

    Creating basic routes

--- 01-fundamentos-js/src/server.js
+++ 01-fundamentos-js/src/server.js
@@ -1,8 +1,19 @@
 import http from 'node:http'
 
 const server = http.createServer((request, response) => {
-    return response.end('Hello World')
 
+    const {method, url} = request
+
+    if(method === 'GET' && url === '/users'){
+        return response.end('Listagem de usuário')
+    }
+    
+    if(method === 'POST' && url === '/users'){
+        return response.end('Criação de usuário')
+    }
+
+
+    return response.end('Hello World')
 })
 
 server.listen(3333)
