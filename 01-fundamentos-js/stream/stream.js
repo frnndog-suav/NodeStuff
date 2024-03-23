6e45b201f2e5c5cb22bd33d0fb2ec09441f6369b
Fernando Goia <f97.goya@gmail.com>
2024-03-23 11:07:07 -0300

    Readable, Transform and Writeable streams

--- 01-fundamentos-js/stream/stream.js
+++ 01-fundamentos-js/stream/stream.js
@@ -1,21 +1,37 @@
-import {Readable} from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
-
     index = 1
 
-    _read(){
-        const i = this.index ++
-        
-        if(i > 100){
-            this.push(null)
-        }else {
-            const buf = Buffer.from(String(i))
-            this.push(buf)
-        }
+    _read() {
+        const i = this.index++
 
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+                this.push(buf)
+            }
+        }, 1000)
     }
-
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class MultiplyByTenStream extends Writable {
+    _write(chunck, enconding, callback) {
+        console.log(Number(chunck.toString()) * 10)
+        callback()
+    }
+}
+
+
+class InverseNumberStream extends Transform {
+    _transform(chunck, enconding, callback) {
+        const transformed = Number(chunck.toString()) * -1
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+new OneToHundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
