f7e7ca77dbd43d90acb557e01e35e7c9f0d09946
Fernando Goia <f97.goya@gmail.com>
2024-03-24 19:26:17 -0300

    Creating temporary database in class

--- 01-fundamentos-js/src/server.js
+++ 01-fundamentos-js/src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
 import { json } from '../middlewares/json.js'
+import { Database } from '../database/database.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
@@ -9,6 +10,7 @@ const server = http.createServer(async (req, res) => {
     //============
 
     if (method === 'GET' && url === '/users') {
+        const users = database.select('users')
         return res.end(JSON.stringify(users))
     }
 
@@ -16,11 +18,19 @@ const server = http.createServer(async (req, res) => {
 
         const { name, email } = req.body
 
-        users.push({
-            id: String(users.length + 1),
+
+        const users = database.select('users')
+
+        const userCount = users?.length ?? 0
+
+        const user = {
+            id: String(userCount + 1),
             name,
             email
-        })
+        }
+
+        database.insert('users', user)
+
         return res.writeHead(201).end()
     }
 
