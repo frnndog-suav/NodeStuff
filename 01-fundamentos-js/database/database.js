f27835748d728aca9dd13981d405eb85b82ab1f7
Fernando Goia <f97.goya@gmail.com>
2024-03-24 19:36:59 -0300

    Database in file format

--- 01-fundamentos-js/database/database.js
+++ 01-fundamentos-js/database/database.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
     #database = {}
+
+    constructor() {
+        fs.readFile(databasePath, 'utf-8').then((data) => {
+            this.#database = JSON.parse(data)
+        }).catch(() => {
+            this.#persist()
+        })
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -9,13 +25,14 @@ export class Database {
 
     insert(table, data) {
 
-        if (Array.isArray(this.database[table])) {
+        if (Array.isArray(this.#database[table])) {
             this.#database[table].push(data)
         } else {
             this.#database[table] = [data]
         }
 
-        return data
+        this.#persist()
 
+        return data
     }
 }
