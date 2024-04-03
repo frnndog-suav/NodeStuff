32ad96a7020bebdfc4a2fd1d2be9832141838def
Fernando Goia <f97.goya@gmail.com>
2024-04-02 22:03:23 -0300

    Writing migrations

--- 02-basico-api-REST/src/database.ts
+++ 02-basico-api-REST/src/database.ts
@@ -1,4 +1,11 @@
-import { knex as setUpKnex } from 'knex'
-import { config } from '../knexfile'
+import { knex as setUpKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite3',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+}
 
 export const knex = setUpKnex(config)
