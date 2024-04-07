385785d396d21ad6e9ec03bdf15855e3df800ebd
Fernando Goia <f97.goya@gmail.com>
2024-04-07 18:42:48 -0300

    Finishing test implementations

--- 02-basico-api-REST/src/test/transactions.test.ts
+++ 02-basico-api-REST/src/test/transactions.test.ts
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process'
 import request from 'supertest'
-import { afterAll, beforeAll, describe, expect, it, beforeEach } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { app } from '../app'
 
 describe('Transactions routes', () => {
@@ -21,8 +21,6 @@ describe('Transactions routes', () => {
     const response = await request(app.server)
       .post('/transactions')
       .send({ title: 'new transaction', amount: 5000, type: 'credit' })
-
-    console.log('========> response', response)
 
     expect(response.statusCode).toStrictEqual(201)
   })
@@ -45,5 +43,59 @@ describe('Transactions routes', () => {
     expect(listResponse.body.transactions).toStrictEqual([
       expect.objectContaining({ title: 'new transaction', amount: 5000 })
     ])
+  })
+
+  it('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'new transaction', amount: 5000, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    if (!cookies) {
+      throw new Error('No cookie available')
+    }
+
+    const listResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    const transactionId = listResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+
+    expect(getTransactionResponse.body.transaction).toStrictEqual(
+      expect.objectContaining({
+        title: 'new transaction',
+        amount: 5000
+      })
+    )
+  })
+
+  it('should be able to get summary', async () => {
+    const createTransaction1Response = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'credit transaction', amount: 5000, type: 'credit' })
+
+    const cookies = createTransaction1Response.get('Set-Cookie')
+
+    if (!cookies) {
+      throw new Error('No cookie available')
+    }
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({ title: 'debit transaction', amount: 3000, type: 'debit' })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+
+    expect(summaryResponse.body.summary).toStrictEqual({
+      amount: 2000
+    })
   })
 })
