e69ba6e18c9957aab264b4f443e2df1098da3053
Fernando Goia <f97.goya@gmail.com>
2024-04-21 10:30:58 -0300

    Unit test

--- 03-solid/src/use-cases/registerUser/index.test.ts
+++ 03-solid/src/use-cases/registerUser/index.test.ts
@@ -1,5 +1,36 @@
-import { test, expect } from 'vitest'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { RegisterUserUseCase } from '.'
 
-test('check if it works', () => {
-    expect(1).toStrictEqual(1)
+describe('RegisterUserUseCase', () => {
+    it('should hash user password upon registration', async () => {
+        const registerUseCase = new RegisterUserUseCase({
+            async create(data) {
+                return {
+                    id: 'user',
+                    created_at: new Date(),
+                    email: data.email,
+                    name: data.name,
+                    password_hash: data.password_hash,
+                }
+            },
+
+            async findByEmail(email) {
+                return null
+            },
+        })
+
+        const { user } = await registerUseCase.execute({
+            name: 'Test',
+            email: 'test@gmail.com',
+            password: '123456',
+        })
+
+        const isPasswordCorrectlyHashed = await compare(
+            '123456',
+            user.password_hash
+        )
+
+        expect(isPasswordCorrectlyHashed).toStrictEqual(true)
+    })
 })
