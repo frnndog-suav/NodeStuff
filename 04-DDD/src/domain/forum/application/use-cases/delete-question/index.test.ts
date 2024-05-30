6392b147cb466656833e5557d6e1901bae05cf8f
Fernando Goia <f97.goya@gmail.com>
2024-05-30 14:24:04 -0300

    Delete question use case

---
+++ 04-DDD/src/domain/forum/application/use-cases/delete-question/index.test.ts
@@ -0,0 +1,50 @@
+import { UniqueEntityID } from '@/domain/forum/enterprise/entities/value-objects/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { DeleteQuestionUseCase } from '.'
+
+let inMemoryRepository: InMemoryQuestionsRepository
+let useCase: DeleteQuestionUseCase
+
+describe('[Use Case] - Delete question', () => {
+    beforeEach(() => {
+        inMemoryRepository = new InMemoryQuestionsRepository()
+        useCase = new DeleteQuestionUseCase(inMemoryRepository)
+    })
+
+    it('it should be able to delete a question', async () => {
+        const newQuestion = makeQuestion(
+            {
+                authorId: new UniqueEntityID('author-1'),
+            },
+            new UniqueEntityID('question-1')
+        )
+
+        inMemoryRepository.create(newQuestion)
+
+        await useCase.execute({
+            questionId: 'question-1',
+            authorId: 'author-1',
+        })
+
+        expect(inMemoryRepository.items).toHaveLength(0)
+    })
+
+    it('it should not be able to delete a question from another author', async () => {
+        const newQuestion = makeQuestion(
+            {
+                authorId: new UniqueEntityID('author-2'),
+            },
+            new UniqueEntityID('question-1')
+        )
+
+        inMemoryRepository.create(newQuestion)
+
+        await expect(() => {
+            return useCase.execute({
+                questionId: 'question-1',
+                authorId: 'author-1',
+            })
+        }).rejects.toBeInstanceOf(Error)
+    })
+})
