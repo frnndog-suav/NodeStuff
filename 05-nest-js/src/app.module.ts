64dea2d20e4b5d5d51a975dfb1535ca27f056f7f
Fernando Goia <f97.goya@gmail.com>
2024-06-21 18:27:36 -0300

    Configuring authentication

--- 05-nest-js/src/app.module.ts
+++ 05-nest-js/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 
-import { CreateAccountController } from './controllers/create-account/index.controller'
-import { PrismaService } from './prisma/prisma.service'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { CreateAccountController } from './controllers/create-account/index.controller'
 import { envSchema } from './env'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [
+    AuthModule,
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
