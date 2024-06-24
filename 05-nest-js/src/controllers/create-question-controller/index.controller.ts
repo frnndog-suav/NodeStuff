0231bd81d91a3764666fc92488e4cf3958bef28a
Fernando Goia <f97.goya@gmail.com>
2024-06-24 19:01:33 -0300

    Implementing question creation controller

--- 05-nest-js/src/controllers/create-question-controller/index.controller.ts
+++ 05-nest-js/src/controllers/create-question-controller/index.controller.ts
@@ -1,16 +1,49 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(AuthGuard('jwt'))
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log('user', user)
-    return 'oke'
+  async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { content, title } = body
+    const { sub: userId } = user
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug: this.stringToSlug(title),
+      },
+    })
+  }
+
+  private stringToSlug(title: string): string {
+    return title
+      .normalize('NFD') // Decompose accented characters
+      .replace(/[\u0300-\u036f]/g, '') // Remove diacritical marks
+      .replace(/[^a-zA-Z0-9\s-]/g, '') // Remove non-alphanumeric characters (except spaces and hyphens)
+      .trim() // Trim leading/trailing whitespace
+      .replace(/\s+/g, '-') // Replace spaces with hyphens
+      .toLowerCase() // Convert to lowercase
   }
 }
