83f3a247bf7dc59586a2eef7f08d65d5ceface15
Fernando Goia <f97.goya@gmail.com>
2024-06-26 18:46:13 -0300

    Adding previous module project domain and core folders

---
+++ 05-nest-js/src/domain/forum/enterprise/entities/student/index.ts
@@ -0,0 +1,14 @@
+import { Entity } from '@/core/entities'
+import { UniqueEntityID } from '../value-objects/unique-entity-id'
+
+export type TStudentProps = {
+  name: string
+}
+
+export class Student extends Entity<TStudentProps> {
+  static create(props: TStudentProps, id?: UniqueEntityID) {
+    const student = new Student(props, id)
+
+    return student
+  }
+}
