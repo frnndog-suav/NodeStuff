50242f33966891c7ce1230a47b78638b46469f8c
Fernando Goia <f97.goya@gmail.com>
2024-06-28 18:33:34 -0300

    Encrypter gateway

--- 05-nest-js/src/domain/forum/enterprise/entities/student/index.ts
+++ 05-nest-js/src/domain/forum/enterprise/entities/student/index.ts
@@ -3,9 +3,23 @@ import { UniqueEntityID } from '../value-objects/unique-entity-id'
 
 export type TStudentProps = {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<TStudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(props: TStudentProps, id?: UniqueEntityID) {
     const student = new Student(props, id)
 
