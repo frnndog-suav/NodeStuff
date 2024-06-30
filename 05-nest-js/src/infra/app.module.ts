3c691f6144109cabc1ea98a912f440fac72548f7
Fernando Goia <f97.goya@gmail.com>
2024-06-30 11:13:26 -0300

    Creating env module

--- 05-nest-js/src/infra/app.module.ts
+++ 05-nest-js/src/infra/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common'
 
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
-import { envSchema } from './env'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 import { HttpModule } from './http/http.module'
 
 @Module({
   imports: [
     AuthModule,
     HttpModule,
+    EnvModule,
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
