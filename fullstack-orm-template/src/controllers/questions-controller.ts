43e578bb0f013639762d66ccb2ae0b3aabedf30a
Fernando <frnnd.goia97@gmail.com>
2025-10-24 14:59:50 -0300

    feat (prisma): implementing endpoint to create question

--- fullstack-orm-template/src/controllers/questions-controller.ts
+++ fullstack-orm-template/src/controllers/questions-controller.ts
@@ -1,21 +1,27 @@
-import { Request, Response } from "express"
+import { prisma } from "@/prisma";
+import { Request, Response } from "express";
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 
   async create(request: Request, response: Response) {
-    return response.status(201).json()
+    const { title, content, user_id } = request.body;
+
+    await prisma.question.create({ data: { title, content, userId: user_id } });
+
+    return response.status(201).json();
   }
 
   async update(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 
   async remove(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 }
 
-export { QuestionsController }
+export { QuestionsController };
+
