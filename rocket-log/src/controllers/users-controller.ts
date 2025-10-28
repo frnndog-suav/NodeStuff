5fea605fd31c78107a083dc8e05da4d088c8a57a
Fernando <frnnd.goia97@gmail.com>
2025-10-28 09:45:56 -0300

    chore: encrypting password

--- rocket-log/src/controllers/users-controller.ts
+++ rocket-log/src/controllers/users-controller.ts
@@ -1,16 +1,21 @@
+import { hash } from "bcrypt";
 import { NextFunction, Request, Response } from "express";
 import z from "zod";
 
+const BCRYPT_SALT = 8;
+
 export class UsersController {
-  create(req: Request, res: Response, next: NextFunction) {
+  async create(req: Request, res: Response, next: NextFunction) {
     const bodySchema = z.object({
       name: z.string().trim().min(3),
       email: z.email(),
       password: z.string().min(6),
     });
 
-    const parsedData = bodySchema.parse(req.body);
+    const { email, name, password } = bodySchema.parse(req.body);
 
-    return res.json({ message: "User created successfully", data: parsedData });
+    const hashedPassword = await hash(password, BCRYPT_SALT);
+
+    return res.json({ message: "User created successfully", data: hashedPassword });
   }
 }
