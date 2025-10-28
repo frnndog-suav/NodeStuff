b1351ecd37a18ae84bab09354ad1f0ecfed7e244
Fernando <frnnd.goia97@gmail.com>
2025-10-28 09:43:05 -0300

    chore: validating request body to create user

--- rocket-log/src/controllers/users-controller.ts
+++ rocket-log/src/controllers/users-controller.ts
@@ -1,7 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import z from "zod";
 
 export class UsersController {
   create(req: Request, res: Response, next: NextFunction) {
-    return res.json({ message: "OKay" });
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.email(),
+      password: z.string().min(6),
+    });
+
+    const parsedData = bodySchema.parse(req.body);
+
+    return res.json({ message: "User created successfully", data: parsedData });
   }
 }
