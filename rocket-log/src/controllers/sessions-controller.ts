f35d058795aef29c65e72dda5cec8ff025efa977
Fernando <frnnd.goia97@gmail.com>
2025-10-28 10:01:59 -0300

    chore: validating email and password for login

--- rocket-log/src/controllers/sessions-controller.ts
+++ rocket-log/src/controllers/sessions-controller.ts
@@ -1,5 +1,34 @@
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/app-error";
+import { compare } from "bcrypt";
 import { NextFunction, Request, Response } from "express";
+import z from "zod";
 
 export class SessionsController {
-  create(req: Request, res: Response, next: NextFunction) {}
+  async create(req: Request, res: Response, next: NextFunction) {
+    const bodySchema = z.object({
+      email: z.email(),
+      password: z.string(),
+    });
+
+    const { email, password } = bodySchema.parse(req.body);
+
+    const userWithSameEmail = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!userWithSameEmail) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const passwordMatched = await compare(password, userWithSameEmail.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    
+  }
 }
