c436ac20cd609458979ffb0e5aec7c4197c9db47
Fernando <frnnd.goia97@gmail.com>
2025-10-28 08:43:13 -0300

    chore: adding zod validation in error middleware

--- rocket-log/src/middlewares/error-handler.ts
+++ rocket-log/src/middlewares/error-handler.ts
@@ -1,5 +1,6 @@
 import { AppError } from "@/utils/app-error";
 import { NextFunction, Request, Response } from "express";
+import z, { ZodError } from "zod";
 
 export function errorHandler(
   error: any,
@@ -9,6 +10,12 @@ export function errorHandler(
 ) {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .json({ message: error.message, issues: z.treeifyError(error) });
   }
 
   return res.status(500).json({ message: error.message });
