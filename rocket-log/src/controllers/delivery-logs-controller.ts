75c72d20de4999dc664bc72d02bfdb76d67f6e3a
Fernando <frnnd.goia97@gmail.com>
2025-10-29 12:57:32 -0300

    feat (controller): implementing delivery log creation

--- rocket-log/src/controllers/delivery-logs-controller.ts
+++ rocket-log/src/controllers/delivery-logs-controller.ts
@@ -1,7 +1,40 @@
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/app-error";
 import { NextFunction, Request, Response } from "express";
+import z from "zod";
 
 export class DeliveryLogsController {
   async create(request: Request, response: Response, next: NextFunction) {
-    return response.json();
+    const bodySchema = z.object({
+      delivery_id: z.uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.deliver.findUnique({
+      where: {
+        id: delivery_id,
+      },
+    });
+
+    if (!delivery) {
+      throw new AppError("Delivery not found.", 404);
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError(
+        "Current delivery is 'processing'. Change status to 'shipped'."
+      );
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliverId: delivery.id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
