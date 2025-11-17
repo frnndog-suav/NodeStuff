022813a9dac50fa815f060a86633a4c74a112075
fernando.goia <fernando.goia@acqio.com.br>
2025-11-17 15:29:09 -0300

    refactor: updating action entity

--- ddd-review/src/domain/entities/action.ts
+++ ddd-review/src/domain/entities/action.ts
@@ -1,13 +1,22 @@
 import { randomUUID } from "node:crypto";
 
+type TConstructorParams = {
+  id?: string;
+  title: string;
+  editionId?: string;
+  description: string;
+};
+
 export class Action {
   public id: string;
   public title: string;
   public description: string;
+  public editionId: string | undefined;
 
-  constructor(title: string, description: string, id?: string) {
+  constructor({ id, title, description, editionId }: TConstructorParams) {
     this.title = title;
     this.id = id ?? randomUUID();
+    this.editionId = editionId;
     this.description = description;
   }
 }
