22f9cf38e7a0a8ea148300e5ab3e029ad1f59f49
fernando.goia <fernando.goia@acqio.com.br>
2025-11-18 15:25:31 -0300

    refactor: adding type to constructor params of user entity

--- ddd-review/src/domain/entities/user.ts
+++ ddd-review/src/domain/entities/user.ts
@@ -1,11 +1,17 @@
 import { randomUUID } from "node:crypto";
 
+type TConstructorParams = {
+  id?: string;
+  email: string;
+  username: string;
+};
+
 export class User {
   public id: string;
   public username: string;
   public email: string;
 
-  constructor(username: string, email: string, id?: string) {
+  constructor({ username, email, id }: TConstructorParams) {
     this.email = email;
     this.username = username;
     this.id = id ?? randomUUID();
